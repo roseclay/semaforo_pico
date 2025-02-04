eb431738073968af6b2a3d325389a55e9e945442
roseclay <gugufag@gmail.com>
2025-02-04 19:54:36 -0300

    Adicionando implementação completa do semáforo e botão no Raspberry Pi Pico

---
+++ build/include/one_shot.h
@@ -0,0 +1,8 @@
+#ifndef ONE_SHOT_H
+#define ONE_SHOT_H
+
+#include "pico/time.h"
+
+void iniciar_one_shot();
+
+#endif
---
+++ build/include/semaforo.h
@@ -0,0 +1,9 @@
+#ifndef SEMAFORO_H
+#define SEMAFORO_H
+
+#include "pico/time.h"
+
+void inicializar_semaforo();
+void iniciar_semaforo();
+
+#endif
